925ac3f054f6bcb3e56db61723006f5af66b8010
robsevcik <robert.a.sevcik@gmail.com>
2021-07-19 07:56:44 -0700

    Update Program.cs

--- MathandComparison/Program.cs
+++ MathandComparison/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace MathandComparison
+namespace income_comparrison
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Person 1");
+            Console.WriteLine("Hourly Rate?");
+            int rate1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Hours per week?");
+            int hourly1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Person2");
+            Console.WriteLine("Hourly rate?");
+            int rate2 = Convert.ToInt32(Console.ReadLine());
+   
+            Console.WriteLine("Hours per week");
+            int hourly2 = Convert.ToInt32(Console.ReadLine());
+           
+            Console.WriteLine("Anual salary of person 1");
+            int salary1 = rate1 * hourly1;
+            int salary2 = rate2 * hourly2;
+            int Anualsalary1 = salary1 * 52;
+            int Anualsalary2 = salary2 * 52;
+            Console.WriteLine(Anualsalary1);
+            Console.WriteLine("Anual salary of person 2");
+            Console.WriteLine(Anualsalary2);
+            Console.WriteLine("Does Person 1 make more money than person2?");
+            bool totalsalary = Anualsalary1 > Anualsalary2;
+            Console.WriteLine(totalsalary);
+            Console.ReadLine();
+
+
+
         }
     }
 }
+
