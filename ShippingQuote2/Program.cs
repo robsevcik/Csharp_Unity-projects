bc2be31025a4427d3b37cb94dcbde31c0a497b8b
robsevcik <robert.a.sevcik@gmail.com>
2021-07-21 08:41:10 -0700

    Create Program.cs

---
+++ ShippingQuote2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Shipping_quote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express.Please follow the instructions below");
+
+            Console.WriteLine("Please enter package weight");
+            int weight = Convert.ToInt32(Console.ReadLine());
+            if (weight > 50)
+            {
+                Console.WriteLine("Package is to heavy to be shipped via Package Express.Have a good day");
+            }
+            else
+            {
+
+                Console.WriteLine("Please enter package width");
+                int width = Convert.ToInt32(Console.ReadLine());
+
+
+
+                Console.WriteLine("Please enter package height");
+                int height = Convert.ToInt32(Console.ReadLine());
+
+
+
+                Console.WriteLine("Please enter package Length");
+                int length = Convert.ToInt32(Console.ReadLine());
+
+
+                if ((width + height + length) > 50)
+                {
+                    Console.WriteLine("Sorry package is to large for shipping.");
+                }
+
+
+                else
+                {
+                    decimal Cost = (((width * height * length) * weight) / 100);
+
+                    Console.WriteLine($"total is: ${Cost}");
+                }
+            }
+        }
+    }
+}
+
