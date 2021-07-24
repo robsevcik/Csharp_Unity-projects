7bc049bd993279ac1dae803c127c4fd45d955528
robsevcik <robert.a.sevcik@gmail.com>
2021-07-24 11:54:29 -0700

    Create Program.cs

---
+++ Iteration Assignment 2/Program.cs
@@ -0,0 +1,153 @@
+﻿using DocumentFormat.OpenXml.Office.CustomUI;
+using System;
+using System.Collections.Generic;
+
+
+namespace Iteration_Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] bikes = { "harley", "Honda", "Indian", "Yamaha", "Ducati", "Triumph"};
+            //Console.WriteLine("What is your Favorite bike?");
+            //string favoriteBike = Console.ReadLine();
+
+
+
+            //foreach (string name in bikes)
+            //{
+            //    Console.WriteLine(favoriteBike + name);
+            //}
+
+            //foreach (string name1 in bikes)
+            //{
+            //    Console.WriteLine(name1);
+            //}
+            //Console.ReadLine();
+
+            //Step 1............................
+
+            //infinite loop
+
+            //for (int a = 0; a < 50; a--)
+            //{
+            //    Console.WriteLine("value : {0}", a);
+            //}
+            //Console.ReadLine();
+
+
+
+            ////fixed loop
+
+            //for(int a = 0; a < 50; a++)
+            //{
+            //    Console.WriteLine("value : {0}");
+            //}
+
+            //Console.ReadLine();
+
+
+            //step 2..............................
+
+
+
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("loop through");
+            //}
+            //Console.ReadLine();
+
+            //for(int i = 0; i <= 5; i++)
+            //{
+            //    Console.WriteLine("loop through");
+            //}
+            //Console.ReadLine();
+            //step 3...............................
+
+            //List<string> cookie = new List<string>();
+
+            //cookie.Add("chocolate_chip");
+            //cookie.Add("peanutbutter");
+            //cookie.Add("snickerdoodle");
+            //cookie.Add("macadamia");
+            //cookie.Add("oatmeal");
+
+            //Console.WriteLine("what is your favorite cookie?");
+            //string index = Console.ReadLine();
+            //for (int w = 0; w < cookie.Count; w++)
+            //{
+            //    if (cookie[w] == index)
+            //    {
+            //        Console.WriteLine("match found at index " + w);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("sorry thats not the right one");
+
+            //    }
+            //    Console.ReadLine();
+
+
+            //step 4.................................
+
+
+
+            //List<string> fruit = new List<string>();
+
+            //fruit.Add("apple");
+            //    fruit.Add("orange");
+            //    fruit.Add("apple");
+            //    fruit.Add("grapes");
+            //    fruit.Add("banana");
+            //    fruit.Add("apple");
+
+            //    Console.WriteLine("Select text to serch for in list");
+            //    string fruitselection = Console.ReadLine();
+
+
+            //    List<string> fruitCopy = new List<string>();
+
+            //    for (var i = 0; i < 1; i++)
+
+
+            //        if (fruit[i] == fruitselection)
+            //            {
+            //                Console.WriteLine(fruitselection + " has appeared before.");
+            //            }
+            //         else
+            //            {
+            //                Console.WriteLine("Sorry that is not on the list");
+            //            }
+
+
+            //    Console.ReadLine();
+
+            // part 5...............................................
+
+            //List<string> fruit = new List<string>();
+
+            //fruit.Add("apple");
+            //fruit.Add("orange");
+            //fruit.Add("apple");
+            //fruit.Add("grapes");
+            //fruit.Add("banana");
+            //fruit.Add("apple");
+
+            //foreach (string item in fruit)
+            //    if (fruit[0] == item)
+            //    {
+            //        Console.WriteLine(item + " this has already apeared");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(item + " you havent seen this yet");
+            //    }
+            //Console.ReadLine();
+
+
+            //step 6......................................................
+        } 
+    }
+}
