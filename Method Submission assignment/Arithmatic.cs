4bb2fa847d378a45df95c9d77970491634bb8b0c
robsevcik <robert.a.sevcik@gmail.com>
2021-07-27 16:26:20 -0700

    Create Arithmatic.cs

---
+++ Method Submission assignment/Arithmatic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Submission_assignment
+{
+    class Arithmatic
+    {
+        public int MyMethod1(int numone, int numtwo)
+        {
+
+            int i = numone + 5;
+
+            return i;
+        }
+
+        public int MyMethod(int numone)
+        {
+
+            int a = numone * 5;
+
+            return a;
+        }
+
+        public int MyMethod2(int numone)
+        {
+            int j = numone / 2;
+
+            return j;
+        }
+     
+    }
+}
