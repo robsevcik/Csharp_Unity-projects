98ec7a040541681b12fcf36626321f0ff5361d8f
robsevcik <robert.a.sevcik@gmail.com>
2021-07-29 08:58:04 -0700

    Create Program.cs

---
+++ Main method submission assignment/Main method submission assignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Main_method_submission_assignment
+{
+    class Class1
+    {
+        private static void Main()
+        {
+            Math math = new();
+
+            Console.WriteLine("enter a number to multiply by");
+            int numone = Convert.ToInt32(Console.ReadLine());
+
+
+            int d = math.MyMethod1(numone);
+            Console.WriteLine(d);
+
+
+
+            Math math1 = new();
+           
+
+            Console.WriteLine("enter a number to add by");
+            decimal numtwo = Convert.ToDecimal(Console.ReadLine());
+
+
+            decimal b = math.MyMethod1(numtwo);
+            Console.WriteLine(b);
+
+            Math math2 = new();
+
+            Console.WriteLine("enter a number to subtract by");
+            string numthree = Convert.ToInt32(Console.ReadLine());
+
+
+            int c = math.MyMethod1(numthree);
+            Console.WriteLine(c);
+        } 
+    }
+}
+
