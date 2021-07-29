04164a5f104b1f007b41bd7a176aafa6abaed65f
robsevcik <robert.a.sevcik@gmail.com>
2021-07-29 08:55:48 -0700

    Create Class1.cs

---
+++ Main method submission assignment/Main method submission assignment/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Main_method_submission_assignment
+{
+    class Math
+    {
+
+        public int MyMethod1(int numtwo)
+        {
+            int j = Convert.ToInt32(numtwo);
+            int i = j * 5;
+
+            return i;
+        }
+
+        public int MyMethod1(decimal numthree)
+        {
+            int z = Convert.ToInt32(numthree);
+            int m = z + 6;
+
+            return m;
+        }
+
+        public int MyMethod1(string numfour)
+        {
+            int n = Convert.ToInt32(numfour);
+            
+           int x = n - 8;
+
+            return x;
+        }
+    }
+
+    }
