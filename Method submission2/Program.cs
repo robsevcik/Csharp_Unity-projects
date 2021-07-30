7794833eeee3a6a68e0397ea37d5ab5b66c0280d
robsevcik <robert.a.sevcik@gmail.com>
2021-07-30 12:06:11 -0700

    Create Program.cs

---
+++ Method submission2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Method_submission2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            optional1 optional1 = new optional1();
+
+            Console.WriteLine("input a number");
+            int v = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("input a second number or nothing");
+           string y = Console.ReadLine();
+
+             if (y == "")
+            {
+                optional1.AddNumbers(v);
+
+
+            }
+             else
+            {
+                int x = Convert.ToInt32(y);
+                optional1.AddNumbers(x, v);
+               
+            }
+        }
+    }
+}
