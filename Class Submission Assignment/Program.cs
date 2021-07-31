84ba515cfbe48075ede3d5fa52a7e8fb71b0a276
robsevcik <robert.a.sevcik@gmail.com>
2021-07-31 11:51:18 -0700

    Create Program.cs

---
+++ Class Submission Assignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Class_Submission_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instatiation object of class 1 named obj 1//
+            Class1 obj1 = new Class1();
+            //display a message, read user input into int//
+            Console.WriteLine(" enter a number");
+            int y = Convert.ToInt32(Console.ReadLine());
+            //calls the method1 method through obj1//
+            obj1.Method1(y);
+            int num;
+            //calls the method2 method through obj1//
+            obj1.Method2(out num);
+            Console.WriteLine(num);
+            string text = " a value";
+            //calls overloaded version of method1//
+            obj1.Method1(text);
+            Class2.Method1();
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
