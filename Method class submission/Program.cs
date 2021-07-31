814b09b57bca609f81c9dad197b577ab3448fea5
robsevcik <robert.a.sevcik@gmail.com>
2021-07-31 09:40:20 -0700

    Create Program.cs

---
+++ Method class submission/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_class_submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+
+            Console.WriteLine("enter a number");
+            int numone = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter aother number");
+            int numtwo = Convert.ToInt32(Console.ReadLine());
+
+
+             Class1.Method(numone, numtwo);
+
+            Class1.Method(parameter1: numone, parameter2: numtwo);
+        }
+    }
+}
