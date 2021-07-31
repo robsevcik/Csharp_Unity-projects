fea343d0cb8573a8c752fffb8929654bf4826e45
robsevcik <robert.a.sevcik@gmail.com>
2021-07-31 09:40:42 -0700

    Create Class1.cs

---
+++ Method class submission/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_class_submission
+{
+    class Class1
+    {
+       public static void Method(int numone, int numtwo)
+        {
+            int i = numone + 5;
+
+            Console.WriteLine(numtwo);
+        }
+    }
+}
