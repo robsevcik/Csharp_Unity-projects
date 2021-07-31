6fdd14e80f48840cb8eb43bfad785d2e10fe6f7a
robsevcik <robert.a.sevcik@gmail.com>
2021-07-31 11:49:51 -0700

    Create Class1.cs

---
+++ Class Submission Assignment/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Submission_Assignment
+{
+    public class Class1
+    {
+      public void Method1(int x)
+      {
+        int result = x / 2;
+        Console.WriteLine(result);
+
+      }
+
+        public void Method2(out int y)
+        {
+
+            y = 13;
+
+        }
+        public void Method1(string b)
+        {
+            Console.WriteLine("the string is" + b);
+        }
+    }
+ 
+
+}
