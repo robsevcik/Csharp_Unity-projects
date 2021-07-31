0aaab45105f5b154d0d67a7f7ed76a7ca4d1589c
robsevcik <robert.a.sevcik@gmail.com>
2021-07-31 11:50:08 -0700

    Create Class2.cs

---
+++ Class Submission Assignment/Class2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Submission_Assignment
+{
+   public static class Class2
+    {
+        public static void Method1()
+        {
+            Console.WriteLine("the static class method has been called");
+
+
+        }
+
+
+    }
+}
