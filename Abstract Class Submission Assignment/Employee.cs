221a792bf4d631e04eb3e9fb36a573d8dee5013e
robsevcik <robert.a.sevcik@gmail.com>
2021-08-01 13:21:11 -0700

    Create Employee.cs

---
+++ Abstract Class Submission Assignment/Employee.cs
@@ -0,0 +1,15 @@
+﻿using DocuSign.eSign.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class_Submission_Assignment
+{
+   public class Employee: Person
+    {
+
+        Person.SayName(firstName, lastName);
+    }
+}
