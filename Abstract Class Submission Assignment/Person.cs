d229e925fdbe98756e5690d86f258777aad6bd4f
robsevcik <robert.a.sevcik@gmail.com>
2021-08-01 13:21:43 -0700

    Create Person.cs

---
+++ Abstract Class Submission Assignment/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class_Submission_Assignment
+{
+    public abstract class Person
+    {
+        string firstName { get; set; }
+        string lastName { get; set; }
+    }
+    public void SayName();
+    {
+     Console.WriteLine(firstname + lastname);
+        }
+}
