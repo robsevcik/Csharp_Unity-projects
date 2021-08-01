6a48fa5ea6c9dc9267286542d9145fee6ee6ba70
robsevcik <robert.a.sevcik@gmail.com>
2021-08-01 14:24:56 -0700

    Update Employee.cs

--- Abstract Class Submission Assignment/Employee.cs
+++ Abstract Class Submission Assignment/Employee.cs
@@ -7,9 +7,17 @@ using System.Threading.Tasks;
 
 namespace Abstract_Class_Submission_Assignment
 {
-   public class Employee: Person
+   public class Employee: Person, IQuittable//inherit the interface as many as you want//
     {
-
-        Person.SayName(firstName, lastName);
+        public override void SayName()
+        {
+            Console.WriteLine(Firstname + " " + Lastname);
+        }
+       
+        public void Quit()
+        {
+            Console.WriteLine("test text1");
+        }
+       
     }
 }
