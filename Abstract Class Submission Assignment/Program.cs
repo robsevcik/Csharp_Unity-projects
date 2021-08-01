86189c3fbd1ebc49e67850a4753979e13862a0a9
robsevcik <robert.a.sevcik@gmail.com>
2021-08-01 13:21:38 -0700

    Create Program.cs

---
+++ Abstract Class Submission Assignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Abstract_Class_Submission_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+
+            Employee.SayName();
+        }
+    }
+}
