45ae4307b932fd71342f56c026537d181522b7e7
robsevcik <robert.a.sevcik@gmail.com>
2021-08-01 11:58:57 -0700

    Create Employee.cs

---
+++ Methods and objects submission assignment/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_and_objects_submission_assignment
+{//how to inherit//
+    class Employee : Person
+    {
+        public int Id { get; set; }
+    }
+}
