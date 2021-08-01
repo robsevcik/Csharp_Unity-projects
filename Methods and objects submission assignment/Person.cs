a7832af230075f268c82461d483e693069132cc0
robsevcik <robert.a.sevcik@gmail.com>
2021-08-01 12:00:21 -0700

    Create Person.cs

---
+++ Methods and objects submission assignment/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_and_objects_submission_assignment
+{
+    class Person
+    {// properties of the class//
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+
+        public void SayName()
+        {
+        
+            Console.WriteLine(Firstname + Lastname);
+        }
+    }
+}
