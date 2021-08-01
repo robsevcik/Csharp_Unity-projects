39bafc8e4f94a921059bec0093ddd8592b0a2769
robsevcik <robert.a.sevcik@gmail.com>
2021-08-01 14:58:43 -0700

    Update Program.cs

--- Abstract Class Submission Assignment/Program.cs
+++ Abstract Class Submission Assignment/Program.cs
@@ -2,22 +2,16 @@
 
 namespace Abstract_Class_Submission_Assignment
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Employee employee = new Employee();
-            Employee obj1 = new Employee();
 
-            obj1.Firstname = "Sample";
-            obj1.Lastname = "Student";
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
 
-            obj1.SayName();
-            //create an object with polymorphism//
-            IQuittable obj2 = new Employee();
-
-            // call a method on the object//
-            obj2.Quit();
+            Employee.SayName();
         }
     }
 }
