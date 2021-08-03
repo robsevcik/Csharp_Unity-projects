d1fb48070308c36f8a1453dcb6bff7818cfdb409
robsevcik <robert.a.sevcik@gmail.com>
2021-08-03 16:36:34 -0700

    Create IQuittable.cs

---
+++ Abstract class submission assignment2/IQuittable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_class_submission_assignment2
+{
+    interface IQuittable
+    {
+       void Quit(Player player);
+    }
+}
