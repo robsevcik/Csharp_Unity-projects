0d6e3fe31fa03d95e1577f5f189eab1201b76f02
robsevcik <robert.a.sevcik@gmail.com>
2021-08-03 16:36:38 -0700

    Update Employee.cs

--- Abstract class submission assignment2/Employee.cs
+++ Abstract class submission assignment2/Employee.cs
@@ -2,13 +2,18 @@
 
 namespace Abstract_Class_Submission_Assignment
 {
-    public class Employee : Persons
+    public class Employee : Persons, IQuittable
     {
         public override void SayName()
         {
             Console.WriteLine(firstName + lastName);
         }
 
+        public void Quit(Player player)
+        {
+            Console.WriteLine("Its working");
+        }
 
+        Object obj = new IQuittable();
     }
 }
