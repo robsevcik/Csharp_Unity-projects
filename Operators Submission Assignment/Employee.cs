bd5790a82b886655b233e254a148cce36a2490b3
robsevcik <robert.a.sevcik@gmail.com>
2021-08-04 16:50:16 -0700

    Create Employee.cs

---
+++ Operators Submission Assignment/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Submission_Assignment
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+
+
+        public static bool operator ==(Employee obj1, Employee obj2)
+        {
+        return obj1 == obj2;
+        }
+    public static bool operator !=(Employee obj1, Employee obj2)
+    {
+    return obj1 != obj2;
+
+    }
+  }
+}
