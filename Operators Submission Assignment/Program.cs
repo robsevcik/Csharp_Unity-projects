723e81e44955c2e446f2eca8f25d28109b552ca3
robsevcik <robert.a.sevcik@gmail.com>
2021-08-04 16:54:23 -0700

    Create Program.cs

---
+++ Operators Submission Assignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Operators_Submission_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee obj1 = new Employee();
+            Employee obj2 = new Employee();
+
+            obj1.ID = (5);
+            obj2.ID = (6);
+
+            Console.WriteLine(5 == 6);
+            Console.ReadLine();
+        }
+
+        
+        
+
+    }
+}
