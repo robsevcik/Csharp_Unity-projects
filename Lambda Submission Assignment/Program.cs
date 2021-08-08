f1532fd2163197a42672974972b9670d78b10d1d
robsevcik <robert.a.sevcik@gmail.com>
2021-08-08 16:52:53 -0700

    Update Program.cs

--- Lambda Submission Assignment/Program.cs
+++ Lambda Submission Assignment/Program.cs
@@ -6,22 +6,27 @@ namespace Lambda_Submission_Assignment
     {
         public static void Main(string[] args)
         {
-            /*List<string> employee = new List<string>()*/;
+            /*List<string> employee = new List<string>()*/
+            ;
 
 
             int counter = 0;
             foreach (string Joe in Employee)
             {
-                if (string.Joe = string Joe)
-                        {
-                    Console.WriteLine(Joe);
-                        }  
+                if (Employee.FirstName == FirstName.Joe)
+                {
+                    counter++;
+
+                }
 
             }
+            Console.WriteLine(counter);
 
             string count = Employee.count(x => x.Employee == firstName.Joe);
 
-            List<int> employeeList = new List<int>() = {employeeList.where(x => Id > 5).ToList};
+            List<int> employeeList = new List<int>() = { employeeList.where(x => Id > 5).ToList};
+            Console.ReadLine();
         }
+
     }
 }
