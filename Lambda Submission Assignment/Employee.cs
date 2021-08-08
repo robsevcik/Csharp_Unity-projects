0aaaeb6cb2ade614309364f74c9ddfcd44f1ad02
robsevcik <robert.a.sevcik@gmail.com>
2021-08-08 12:34:58 -0700

    Create Employee.cs

---
+++ Lambda Submission Assignment/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_Submission_Assignment
+{
+    class Employee
+    {
+        List<Employee> Employees = new List<Employee>();
+
+        Employees() firstName = "Joe", lastName = "Swanson", Id = 001);
+        Employees() { firstName = "Joe", lastName = "Strummer", Id = 002}
+         Employees() { firstName = "Joe", lastName = "Johnson", Id = 003}
+         Employees() { firstName = "Ian", lastName = "Curtis", Id = 004}
+         Employees() { firstName = "Ian", lastName = "Mcay", Id = 005}
+         Employees() { firstName = "Nicky", lastName = "Six", Id = 006}
+         Employees() { firstName = "Nick", lastName = "Mars", Id = 007}
+        Employees() { firstName = "Tommy", lastName = "Lee", Id = 008}
+         Employees() { firstName = "Vince", lastName = "Niel", Id = 009}
+     Employees () { firstName = "Brandon", lastName = "Lee", Id = 010}
+    }
+}
