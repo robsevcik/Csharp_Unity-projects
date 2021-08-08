c5c346a95c8f48dd9c72e408a2dcdc7e20116e36
robsevcik <robert.a.sevcik@gmail.com>
2021-08-08 12:34:32 -0700

    Create Program.cs

---
+++ Lambda Submission Assignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lambda_Submission_Assignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*List<string> employee = new List<string>()*/;
+
+
+            int counter = 0;
+            foreach (string Joe in Employee)
+            {
+                if (string.Joe = string Joe)
+                        {
+                    Console.WriteLine(Joe);
+                        }  
+
+            }
+
+            string count = Employee.count(x => x.Employee == firstName.Joe);
+
+            List<int> employeeList = new List<int>() = {employeeList.where(x => Id > 5).ToList};
+        }
+    }
+}
