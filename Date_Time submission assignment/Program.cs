38c7a2514d67e80bbae3c4e158c2a5668b34a240
robsevcik <robert.a.sevcik@gmail.com>
2021-08-14 15:02:42 -0700

    Create Program.cs

---
+++ Date_Time submission assignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+
+
+namespace Date_Time_submission_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            Console.WriteLine(DateTime.Now);
+
+            Console.WriteLine("Enter a Number please");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            DateTime currenttime = DateTime.Now;
+            currenttime = Convert.ToInt32(Console.ReadLine();
+  
+            TimeSpan finalTime = number + currenttime;
+        }
+
+        
+
+
+    }
+    
+}
